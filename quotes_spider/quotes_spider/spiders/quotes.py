3f39c0aa79a1aa9666a40c08dc7fd167c5646037
ahmetfteke <ateke1@binghamton.edu>
2017-04-28 07:28:30 -0400

    first spider

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -5,7 +5,10 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     allowed_domains = ["http://quotes.toscrape.com/"]
-    start_urls = ['http://http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        h1_tag = response.xpath('//h1/a/text()').extract_first()
+        print(h1_tag)
+        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        print(tags)
